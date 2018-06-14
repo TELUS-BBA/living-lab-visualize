60eda68e6a53428ce2d8aba9dbbd1d9229fe7ac9
Adam Pickering <adamkpickering@gmail.com>
2018-06-14 17:14:04 -0600

    Moved bandwidth dataframe fetching code into bandwidth.py; added print() statements to provide some feedback to user while they wait for it to complete

--- bandwidth.py
+++ bandwidth.py
@@ -2,170 +2,67 @@
 
 import requests
 from getpass import getpass
+import pandas as pd
+import numpy as np
 import matplotlib
 matplotlib.use('svg')
-import matplotlib.pyplot as plot
-import numpy as np
-from statistics import mean
-import maya
-from datetime import timedelta
-from copy import copy
-
-from common import get_data
+import matplotlib.pyplot as plt
+import json
+from common import get_from_api
 
 
-BASE_URL = "http://localhost:5000"
-NANOPI_URL = "{}/nanopi/".format(BASE_URL)
-IPERF3_URL = "{}/iperf3/".format(BASE_URL)
+def get_bandwidth_dataframe(auth=None, params=None):
 
+    base_url = "http://localhost:5000"
+    iperf3_url = "{}/iperf3/".format(base_url)
 
-def compare_average_bandwidth(file, nanopis, username, password):
-    """Generate a chart that compares the bandwidth results of the given list of nanopis.
-    
-    Arguments:
-    file - the file name that the chart will be saved into, for example 'bandwidth.svg'
-    nanopis - a list of nanopi dicts as given in the REST API
-    username - the HTTP basic auth username that lets you access the API
-    password - the HTTP basic auth password that lets you access the API
-    """
-    auth = requests.auth.HTTPBasicAuth(username, password)
+    if not auth:
+        username = input("API Username: ")
+        password = getpass(prompt="API Password: ")
+        auth = requests.auth.HTTPBasicAuth(username, password)
 
-    bandwidths = []
-    for nanopi in nanopis:
-        print("Fetching data for nanopi {}".format(nanopi.get('id')))
+    # get list of results from API with given parameters
+    print("Getting raw data from API...")
+    results = get_from_api(iperf3_url, auth, params)
 
-        # get average up bandwidth for nanopi
-        params = {
-            'nanopi': nanopi.get('id'),
-            'direction': 'up',
-        }
-        up_results = get_from_api(IPERF3_URL, auth, params)
-        up_bandwidths = [result.get('bandwidth') for result in up_results]
-        mean_up_bandwidth = mean(up_bandwidths)
+    # put initial multiindex together
+    print("Putting initial dataframe together...")
+    for result in results:
+        result['upload_date'] = pd.Timestamp(result.get('upload_date')).tz_convert('America/Edmonton')
+    index_tuples = [[x.get('upload_date').floor('H'), x.get('nanopi'), x.get('direction')] for x in results]
+    index = pd.MultiIndex.from_tuples(index_tuples, names=['datetime', 'nanopi', 'direction'])
 
-        # get average down bandwidth for nanopi
-        params = {'nanopi': nanopi.get('id'), 'direction': 'down'}
-        results = get_from_api(iperf3_url, auth, params)
-        down_bandwidths = [result.get('bandwidth') for result in results]
-        mean_down_bandwidth = mean(down_bandwidths)
+    # parse bulk of data
+    df = pd.DataFrame({'id': [x.get('id') for x in results],
+                       'bandwidth': [x.get('bandwidth') for x in results],
+                       'upload_date': [x.get('upload_date') for x in results]},
+                      index=index)
 
-        bandwidths.append({
-            'nanopi': nanopi.get('id'),
-            'location': nanopi.get('location_info'),
-            'up': mean_up_bandwidth,
-            'down': mean_down_bandwidth,
-        })
+    # remove duplicates
+    print("Removing duplicates...")
+    df1 = df.loc[~df.index.duplicated(keep='last'), :]
 
-    # report average bandwidths for each nanopi
-    for bandwidth in bandwidths:
-        print("At {} average up is {} Mbit/s and average down is {} Mbit/s".format(
-            bandwidth.get('location'),
-            round(bandwidth.get('up')),
-            round(bandwidth.get('down'))
-        ))
+    # reindex to highlight missing data
+    print("Re-indexing dataframe...")
+    start = df.index.get_level_values('datetime')[0]
+    end = df.index.get_level_values('datetime')[-1]
+    iterables = [
+        pd.date_range(start, end=end, freq='H'),
+        set(df.index.get_level_values('nanopi')),
+        ['up', 'down']
+    ]
+    new_index = pd.MultiIndex.from_product(iterables, names=['datetime', 'nanopi', 'direction'])
+    df2 = df1.reindex(index=new_index)
 
-    bar_width = 0.4
-    opacity = 0.6
-    figure, axis = plot.subplots()
+    return df2
 
-    index = range(len(bandwidths))
-    labels = [bandwidth.get('location') for bandwidth in bandwidths]
-    means_up = [bandwidth.get('up') for bandwidth in bandwidths]
-    means_down = [bandwidth.get('down') for bandwidth in bandwidths]
-
-    rects_up = axis.bar(index, means_up, bar_width, label='Up', color='r', alpha=opacity)
-    rects_down = axis.bar([a + bar_width for a in index], means_down,
-                          bar_width, label='Down', color='b', alpha=opacity)
-    axis.set_xlabel("Location")
-    axis.set_ylabel("Bandwidth (Mbit/s)")
-    axis.set_title("Average Bandwidth by Location")
-    axis.set_xticks([a + bar_width / 2 for a in index])
-    axis.set_xticklabels(labels)
-    axis.legend()
-    figure.set_size_inches(12, 6)
-    figure.savefig(file)
-
-
-def average_bandwidth_per_hour(file, nanopis, username, password):
-    """Generate a chart that shows the bandwidth per hour for an average of the given list of nanopis.
-    
-    Arguments:
-    file - the file name that the chart will be saved into, for example 'bandwidth.svg'
-    nanopis - a list of nanopi dicts as given in the REST API
-    username - the HTTP basic auth username that lets you access the API
-    password - the HTTP basic auth password that lets you access the API
-    """
-    auth = requests.auth.HTTPBasicAuth(username, password)
-
-    # get up data
-    up_data = {hour: [] for hour in range(24)}
-    for nanopi in nanopis:
-        print("Fetching data for nanopi {}".format(nanopi.get('id')))
-        params = {
-            'nanopi': nanopi.get('id'),
-            'direction': 'up',
-        }
-        results = get_data(IPERF3_URL, auth, params)
-        for result in results:
-            up_data[result.get('upload_date').hour].append(result.get('bandwidth'))
-
-    for key, value in up_data.items():
-        up_data[key] = mean(value)
-        print("Hour {}: {} Mbit/s".format(key, round(up_data[key])))
-
-    # get down data
-    down_data = {hour: [] for hour in range(24)}
-    for nanopi in nanopis:
-        print("Fetching data for nanopi {}".format(nanopi.get('id')))
-        params = {
-            'nanopi': nanopi.get('id'),
-            'direction': 'down',
-        }
-        results = get_data(IPERF3_URL, auth, params)
-        for result in results:
-            down_data[result.get('upload_date').hour].append(result.get('bandwidth'))
-
-    for key, value in down_data.items():
-        down_data[key] = mean(value)
-        print("Hour {}: {} Mbit/s".format(key, round(down_data[key])))
-
-    # plot data
-    
-#    figure, axis = plot.subplots()
-#    axis.plot()
-#    axis.set(xlabel='Hour of Day', ylabel='Bandwidth (Mbit/s)', title='About as simple as it gets')
-#    axis.grid()
-#    figure.savefig('graph.svg')
-
-
-def each_bandwidth_per_hour(file, nanopis, username, password):
-    pass
-
-def bandwidth_over_time_period(file, nanopis, username, password):
-    pass
-
-
-#params = {
-#    'nanopi': 12,
-#    'direction': 'down'
-#}
-#results = get_from_api(iperf3_url, auth, params)
-#
-#bandwidth = [result.get('bandwidth') for result in results]
-#
-#figure, axis = plot.subplots()
-#axis.plot(bandwidth)
-#axis.set(xlabel='time', ylabel='bandwidth (Mbit/s)', title='About as simple as it gets')
-#axis.grid()
-#figure.savefig('graph.svg')
 
 if __name__ == '__main__':
 
-    username = input("API Username: ")
-    password = getpass(prompt="API Password: ")
-    auth = requests.auth.HTTPBasicAuth(username, password)
+    df2 = get_bandwidth_dataframe()
 
-    nanopis = requests.get(NANOPI_URL, auth=auth).json()
 
-#    compare_average_bandwidth('bandwidth.svg', username, password)
-    average_bandwidth_per_hour('bandwidth.svg', nanopis, username, password)
+
+
+
+
--- common.py
+++ common.py
@@ -2,20 +2,20 @@
 
 import requests
 from getpass import getpass
+import pandas as pd
+import numpy as np
 import matplotlib
 matplotlib.use('svg')
-import matplotlib.pyplot as plot
-import numpy as np
-from statistics import mean
-import maya
-from datetime import timedelta
-from copy import copy
+import matplotlib.pyplot as plt
+import json
 
 
 def get_from_api(url, auth, params):
     """Pages through the REST API and retrieves all the data for a certain set of parameters.
 
-    Note that the arguments more or less mirror those for the requests library.
+    You can treat this as a regular call to requests.get(...).json();
+    it abstracts the pagination and gives you the same result you'd get without pagination.
+    The arguments more or less mirror those for the requests library.
     See http://docs.python-requests.org/en/master/ for more information.
 
     Arguments:
@@ -40,46 +40,3 @@ def get_from_api(url, auth, params):
         url = json.get('next')
 
     return results
-
-
-def ingest(results, datetime_field):
-    """Given a list of dicts, converts the time fields into MayaDT objects.
-
-    Arguments:
-    results - a list of dicts; each one should contain the key given in datetime_field
-    datetime_field - the key of the field in each dict that contains the maya datetime object
-    """
-    ingested_results = []
-    for result in results:
-        ingested_result = result.copy()
-        maya_time = maya.parse(result.get(datetime_field))
-        ingested_result[datetime_field] = maya_time
-        ingested_results.append(ingested_result)
-
-    return ingested_results
-
-
-def localize(results, timezone, datetime_field):
-    """Converts time of each dict in list of dicts to a new timezone.
-    
-    Note that it is assumed that the times in each datetime_field are currently in UTC.
-    maya should be used for time objects.
-
-    Arguments:
-    results - a list of dicts; each one should contain the key given in datetime_field
-    timezone - the timezone that we want to conver the times to
-    datetime_field - the key of the field in each dict that contains the maya datetime object
-    """
-    localized_results = []
-    for result in results:
-        localized_result = result.copy()
-        localized_result[datetime_field] = result[datetime_field].datetime(to_timezone=timezone)
-        localized_results.append(localized_result)
-    return localized_results
-
-
-def get_data(url, auth, params, timezone='UTC', datetime_field='upload_date'):
-    raw_results = get_from_api(url, auth, params)
-    ingested_results = ingest(raw_results, datetime_field=datetime_field)
-    localized_results = localize(ingested_results, timezone, datetime_field)
-    return localized_results
