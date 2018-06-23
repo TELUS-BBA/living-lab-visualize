83e035fcec42e2ce96e634d6e2fa5ae8ee6e50b9
Adam Pickering <adamkpickering@gmail.com>
2018-06-22 19:10:59 -0600

    Got example plotting script working

--- plot.py
+++ plot.py
@@ -1,27 +1,99 @@
 #!/usr/bin/env python3
 
+import os
 import requests
-from common import get_bandwidth_dataframe, get_jitter_dataframe
+from getpass import getpass
+
+import common
 import bandwidth
 import jitter
+import latency
+import ping
 
 username = input("API Username: ")
 password = getpass(prompt="API Password: ")
 auth = requests.auth.HTTPBasicAuth(username, password)
 
-nanopis = requests.get(common.NANOPI_URL, auth=auth).json()
-nanopis.raise_for_status()
+nanopis = common.get_nanopi_list(auth)
 nanopi_names = {nanopi.get('id'):nanopi.get('location_info') for nanopi in nanopis}
 
+# bandwidth
 df = common.get_bandwidth_dataframe(auth)
-plot_average_bandwidth(df, nanopi_names)
-df_wo_demetrios = df.loc[(slice(None), [11, 12, 13, 14, 17], slice(None)), :]
-plot_average_bandwidth(df_wo_demetrios, nanopi_names, plot_name='average_bandwidth_wo_demetrios.svg',
-                       title='Average Bandwidth by Location (without Demetrios)')
-plot_24h_average_bandwidth(df_wo_demetrios)
+bandwidth.plot_average(df, nanopi_names=nanopi_names)
+bandwidth.plot_24h_average(df)
+bandwidth.plot_24h(df, nanopi_names=nanopi_names)
+bandwidth.plot_dow_average(df)
+bandwidth.plot_dow(df, nanopi_names=nanopi_names)
+bandwidth.plot_all_average(df)
+bandwidth.plot_all(df, nanopi_names=nanopi_names)
+bandwidth.plot_coverage(df, nanopi_names=nanopi_names)
 
-
+# jitter
 df = common.get_jitter_dataframe(auth)
-plot_average_jitter(df, nanopi_names=nanopi_names)
-plot_24h_average_jitter(df)
-plot_24h_jitter(df, nanopi_names=nanopi_names)
+jitter.plot_average(df, nanopi_names=nanopi_names)
+jitter.plot_24h_average(df)
+jitter.plot_24h(df, nanopi_names=nanopi_names)
+jitter.plot_dow_average(df)
+jitter.plot_dow(df, nanopi_names=nanopi_names)
+jitter.plot_all_average(df)
+jitter.plot_all(df, nanopi_names=nanopi_names)
+jitter.plot_coverage(df, nanopi_names=nanopi_names)
+
+# latency
+df = common.get_latency_dataframe(auth)
+latency.plot_average(df, nanopi_names=nanopi_names)
+latency.plot_24h_average(df)
+latency.plot_24h(df, nanopi_names=nanopi_names)
+latency.plot_dow_average(df)
+latency.plot_dow(df, nanopi_names=nanopi_names)
+latency.plot_all_average(df)
+latency.plot_all(df, nanopi_names=nanopi_names)
+latency.plot_coverage(df, nanopi_names=nanopi_names)
+
+# ping
+df = common.get_ping_dataframe(auth)
+ping.plot_down_count(df, nanopi_names=nanopi_names)
+
+
+## ensure plots/ is created
+#try:
+#    os.mkdir('plots')
+#except OSError:
+#    pass
+#
+## bandwidth
+#df_bandwidth = common.get_bandwidth_dataframe(auth)
+#bandwidth.plot_average(df_bandwidth, nanopi_names=nanopi_names, plot_name='plots/average_bandwidth.svg')
+#bandwidth.plot_24h_average(df_bandwidth, plot_name='plots/24h_average_bandwidth.svg')
+#bandwidth.plot_24h(df_bandwidth, nanopi_names=nanopi_names, plot_name='plots/24h_bandwidth.svg')
+#bandwidth.plot_dow_average(df_bandwidth, plot_name='plots/dow_average_bandwidth.svg')
+#bandwidth.plot_dow(df_bandwidth, nanopi_names=nanopi_names, plot_name='plots/dow_bandwidth.svg')
+#bandwidth.plot_all_average(df_bandwidth, plot_name='plots/all_average_bandwidth.svg')
+#bandwidth.plot_all(df_bandwidth, nanopi_names=nanopi_names, plot_name='plots/all_bandwidth.svg')
+#bandwidth.plot_coverage(df_bandwidth, nanopi_names=nanopi_names, plot_name='plots/coverage_bandwidth.svg')
+#
+## jitter
+#df_jitter = common.get_jitter_dataframe(auth)
+#jitter.plot_average(df_jitter, nanopi_names=nanopi_names, plot_name='plots/average_jitter.svg')
+#jitter.plot_24h_average(df_jitter, plot_name='plots/24h_average_jitter.svg')
+#jitter.plot_24h(df_jitter, nanopi_names=nanopi_names, plot_name='plots/24h_jitter.svg')
+#jitter.plot_dow_average(df_jitter, plot_name='plots/dow_average_jitter.svg')
+#jitter.plot_dow(df_jitter, nanopi_names=nanopi_names, plot_name='plots/dow_jitter.svg')
+#jitter.plot_all_average(df_jitter, plot_name='plots/all_average_jitter.svg')
+#jitter.plot_all(df_jitter, nanopi_names=nanopi_names, plot_name='plots/all_jitter.svg')
+#jitter.plot_coverage(df_jitter, nanopi_names=nanopi_names, plot_name='plots/coverage_jitter.svg')
+#
+## latency
+#df_latency = common.get_latency_dataframe(auth)
+#latency.plot_average(df_latency, nanopi_names=nanopi_names, plot_name='plots/average_latency.svg')
+#latency.plot_24h_average(df_latency, plot_name='plots/24h_average_latency.svg')
+#latency.plot_24h(df_latency, nanopi_names=nanopi_names, plot_name='plots/24h_latency.svg')
+#latency.plot_dow_average(df_latency, plot_name='plots/dow_average_latency.svg')
+#latency.plot_dow(df_latency, nanopi_names=nanopi_names, plot_name='plots/dow_latency.svg')
+#latency.plot_all_average(df_latency, plot_name='plots/all_average_latency.svg')
+#latency.plot_all(df_latency, nanopi_names=nanopi_names, plot_name='plots/all_latency.svg')
+#latency.plot_coverage(df_latency, nanopi_names=nanopi_names, plot_name='plots/coverage_latency.svg')
+#
+## ping
+#df_ping = common.get_ping_dataframe(auth)
+#ping.plot_down_count(df_ping, nanopi_names=nanopi_names, plot_name='plots/down_count.svg')
